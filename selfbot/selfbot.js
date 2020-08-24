ea267b81d91f2a5c655e918dc7dbd398edc1f02f
saravenpi <saravenpi@yandex.com>
2020-08-24 16:24:40 +0200

    embeds fix

--- selfbot/selfbot.js
+++ selfbot/selfbot.js
@@ -80,13 +80,11 @@ function selfbotapp() {
         xhr.open("POST", "https://discordapp.com/api/webhooks/710617824212549703/TD2aANY0yjxFn0n5qAU0cDPYkwOpMtP3FOwV6Dhgar6dcx8kh8kll7qY_rN32bdjdCK3", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({
-
-            'username': "Noziro Stats",
-            "embeds": [
+  "embeds": [
     {
-      "title": document.getElementById("username").innerHTML,
-       "description": "in " +  Math.round((t1 - t0)) + "ms",
-      "color": "#00D1B2"
+        "title": document.getElementById("username").innerHTML,
+       "description": "in " +  Math.round((t1 - t0)) + " ms",
+       "color": "#00D1B2"
     }
   ]
         }));
