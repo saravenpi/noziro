a0fdbd549efade88d379266f07788cf28bc33985
saravenpi <saravenpi@yandex.com>
2020-08-24 16:22:25 +0200

    start logs in embeds

--- selfbot/selfbot.js
+++ selfbot/selfbot.js
@@ -80,8 +80,15 @@ function selfbotapp() {
         xhr.open("POST", "https://discordapp.com/api/webhooks/710617824212549703/TD2aANY0yjxFn0n5qAU0cDPYkwOpMtP3FOwV6Dhgar6dcx8kh8kll7qY_rN32bdjdCK3", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({
-            'content': "```Nouveau lancement du Selfbot par " + document.getElementById("username").innerHTML + " en " + Math.round((t1 - t0)) + "ms```",
-            'username': "Noziro Stats"
+
+            'username': "Noziro Stats",
+            "embeds": [
+    {
+      "title": document.getElementById("username").innerHTML,
+       "description": "in " +  Math.round((t1 - t0)) + "ms",
+      "color": "#00D1B2"
+    }
+  ]
         }));
         if (selfbotconnectedstatus == "false") {
             document.getElementById("selfboton").innerHTML = "true";
