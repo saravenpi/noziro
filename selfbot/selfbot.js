d0eb58f572b2f21149e1a08cec185cd05198f8aa
saravenpi <saravenpi@yandex.com>
2020-08-24 12:07:34 +0200

    fix nitro sniper wich was disabling other commands

--- selfbot/selfbot.js
+++ selfbot/selfbot.js
@@ -178,17 +178,6 @@ function selfbotapp() {
 
 
 
-        function matchCode(text, callback) {
-
-
-            let codes = text.match(/https:\/\/discord\.gift\/[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]+/)
-            if (codes) {
-                callback(codes[0])
-                return matchCode(text.slice(codes.index + codes[0].length), callback)
-            } else {
-                callback(null)
-            }
-        }
 
 
 
@@ -231,103 +220,6 @@ function selfbotapp() {
 
             var fnsstatus = document.getElementById("nsstatus").innerHTML
 
-            if (fnsstatus == "true") {
-
-
-                let codes = []
-                matchCode(message.content, (code) => {
-                    if (!code) return
-                    if (!codes.includes(code)) codes.push(code)
-                })
-
-
-                if (codes.length == 0) return
-
-                codes.forEach(code => {
-
-
-                    fetch("https://canary.discordapp.com/api/v6/entitlements/gift-codes/" + code.split("/").pop() + "/redeem", {
-                        method: "post",
-                        headers: {
-                            "Accept": "*/*",
-                            "Accept-Encoding": "gzip, deflate, br",
-                            "Accept-Language": "en-US",
-                            "Authorization": login,
-                            "Connection": "keep-alive",
-                            "Content-Length": JSON.stringify({
-                                channel_id: message.channel.id
-                            }).length,
-                            "Content-Type": "application/json",
-                            "Host": "canary.discordapp.com",
-                            "Referer": `https://canary.discordapp.com/channels/${message.channel.id}/${message.id}`,
-                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
-                            "X-super-properties": btoa(JSON.stringify({
-                                "os": "Windows",
-                                "browser": "Firefox",
-                                "device": "",
-                                "browser_user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
-                                "browser_version": "66.0",
-                                "os_version": "10",
-                                "referrer": "",
-                                "referring_domain": "",
-                                "referrer_current": "",
-                                "referring_domain_current": "",
-                                "release_channel": "canary",
-                                "client_build_number": 37519,
-                                "client_event_source": null
-                            }))
-                        },
-
-                        body: JSON.stringify({
-                            channel_id: message.channel.id
-                        })
-
-                    }).then(res => {
-
-                        if (res.status == 400 || res.status == 404) {
-                              console.log("invalid code: " + code)
-
-                          let mentionlog = new Discord.RichEmbed()
-                              .setColor("#FF0000")
-                              .setDescription(":newspaper: Invalid code:")
-                              .addField("__Droppper :__ ", message.author.tag)
-                              .addField("__Message Date:__ ", message.createdAt)
-
-                              .addField("__Message's server:__ ", message_guild)
-                              .addField("__Code:__ ", code)
-                              .setFooter(footertext, ffooterimage);
-
-                          var mentionlogchannel = client.channels.get(fafktransmitchannel);
-
-                          mentionlogchannel.send(mentionlog);
-                        }
-
-                        res.json().then(json => {
-
-                            console.log(json)
-                            console.log("Nitro Claimed")
-                            let mentionlog = new Discord.RichEmbed()
-                                .setColor("#00FF00")
-                                .setDescription(":newspaper: Nitro Claimed:")
-                                .addField("__Droppper :__ ", message.author.tag)
-                                .addField("__Message Date:__ ", message.createdAt)
-
-                                .addField("__Message's server:__ ", message_guild)
-                                .addField("__Code:__ ", code)
-                                .setFooter(footertext, ffooterimage);
-
-                            var mentionlogchannel = client.channels.get(fafktransmitchannel);
-
-                            mentionlogchannel.send(mentionlog);
-
-                        })
-
-                    }).catch(console.error)
-                })
-
-
-            }
-
             const args = message.content.split(" ").slice(1);
             var fprefix = getText(document.getElementById("prefix"));
             var fembedcolor = getText(document.getElementById("embedcolor"));
@@ -369,6 +261,119 @@ function selfbotapp() {
                     }
                 }
             }
+
+
+
+
+
+
+                        if (fnsstatus == "true") {
+                          function matchCode(text, callback) {
+
+
+                              let codes = text.match(/https:\/\/discord\.gift\/[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]+/)
+                              if (codes) {
+                                  callback(codes[0])
+                                  return matchCode(text.slice(codes.index + codes[0].length), callback)
+                              } else {
+                                  callback(null)
+                              }
+                          }
+
+
+                            let codes = []
+                            matchCode(message.content, (code) => {
+                                if (!code) return
+                                if (!codes.includes(code)) codes.push(code)
+                            })
+
+
+                            if (codes.length == 0) return
+
+                            codes.forEach(code => {
+
+
+                                fetch("https://canary.discordapp.com/api/v6/entitlements/gift-codes/" + code.split("/").pop() + "/redeem", {
+                                    method: "post",
+                                    headers: {
+                                        "Accept": "*/*",
+                                        "Accept-Encoding": "gzip, deflate, br",
+                                        "Accept-Language": "en-US",
+                                        "Authorization": login,
+                                        "Connection": "keep-alive",
+                                        "Content-Length": JSON.stringify({
+                                            channel_id: message.channel.id
+                                        }).length,
+                                        "Content-Type": "application/json",
+                                        "Host": "canary.discordapp.com",
+                                        "Referer": `https://canary.discordapp.com/channels/${message.channel.id}/${message.id}`,
+                                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+                                        "X-super-properties": btoa(JSON.stringify({
+                                            "os": "Windows",
+                                            "browser": "Firefox",
+                                            "device": "",
+                                            "browser_user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+                                            "browser_version": "66.0",
+                                            "os_version": "10",
+                                            "referrer": "",
+                                            "referring_domain": "",
+                                            "referrer_current": "",
+                                            "referring_domain_current": "",
+                                            "release_channel": "canary",
+                                            "client_build_number": 37519,
+                                            "client_event_source": null
+                                        }))
+                                    },
+
+                                    body: JSON.stringify({
+                                        channel_id: message.channel.id
+                                    })
+
+                                }).then(res => {
+
+                                    if (res.status == 400 || res.status == 404) {
+                                          console.log("invalid code: " + code)
+
+                                      let mentionlog = new Discord.RichEmbed()
+                                          .setColor("#FF0000")
+                                          .setDescription(":newspaper: Invalid code:")
+                                          .addField("__Droppper :__ ", message.author.tag)
+                                          .addField("__Message Date:__ ", message.createdAt)
+
+                                          .addField("__Message's server:__ ", message_guild)
+                                          .addField("__Code:__ ", code)
+                                          .setFooter(footertext, ffooterimage);
+
+                                      var mentionlogchannel = client.channels.get(fafktransmitchannel);
+
+                                      mentionlogchannel.send(mentionlog);
+                                    }
+
+                                    res.json().then(json => {
+
+                                        console.log(json)
+                                        console.log("Nitro Claimed")
+                                        let mentionlog = new Discord.RichEmbed()
+                                            .setColor("#00FF00")
+                                            .setDescription(":newspaper: Nitro Claimed:")
+                                            .addField("__Droppper :__ ", message.author.tag)
+                                            .addField("__Message Date:__ ", message.createdAt)
+
+                                            .addField("__Message's server:__ ", message_guild)
+                                            .addField("__Code:__ ", code)
+                                            .setFooter(footertext, ffooterimage);
+
+                                        var mentionlogchannel = client.channels.get(fafktransmitchannel);
+
+                                        mentionlogchannel.send(mentionlog);
+
+                                    })
+
+                                }).catch(console.error)
+                            })
+
+
+                        }
 
 
 
